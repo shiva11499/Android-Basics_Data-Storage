e6c27ba33f198490e915666c71f3dd9eed5df5a3
shiva11499 <gshivansh37@gmail.com>
2020-06-08 13:48:13 +0530

    Database displayed

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -97,21 +97,21 @@ public class CatalogActivity extends AppCompatActivity {
             int weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
 
             // Iterate through all the returned rows in the cursor
-//            while (cursor.moveToNext()) {
-//                // Use that index to extract the String or Int value of the word
-//                // at the current row the cursor is on.
-//                int currentID = cursor.getInt(idColumnIndex);
-//                String currentName = cursor.getString(nameColumnIndex);
-//                String currentBreed = cursor.getString(breedColumnIndex);
-//                int currentGender = cursor.getInt(genderColumnIndex);
-//                int currentWeight = cursor.getInt(weightColumnIndex);
-//                // Display the values from each column of the current row in the cursor in the TextView
-//                displayView.append(("\n" + currentID + " - " +
-//                        currentName + " - " +
-//                        currentBreed + " - " +
-//                        currentGender + " - " +
-//                        currentWeight));
-//            }
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentBreed = cursor.getString(breedColumnIndex);
+                int currentGender = cursor.getInt(genderColumnIndex);
+                int currentWeight = cursor.getInt(weightColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentBreed + " - " +
+                        currentGender + " - " +
+                        currentWeight));
+            }
 
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
