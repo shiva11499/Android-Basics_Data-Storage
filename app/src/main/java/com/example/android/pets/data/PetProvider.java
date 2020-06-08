961f271424b7318619dbcc7e4dded8d99e9d4cef
shiva11499 <gshivansh37@gmail.com>
2020-06-08 15:47:15 +0530

    Manifest changes

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -22,7 +22,7 @@ public class PetProvider extends ContentProvider {
     @Override
     public boolean onCreate() {
         // TODO: Create and initialize a PetDbHelper object to gain access to the pets database.
-        mDbHelper = new PetDbHelper(this);
+//        mDbHelper = new PetDbHelper(this);
         // Make sure the variable is a global variable, so it can be referenced from other
         // ContentProvider methods.
         return true;
